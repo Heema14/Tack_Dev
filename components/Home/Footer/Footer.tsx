de451437759a009c55f256cb8e2a81b7dbe32590
Ibrahim_Yaseen <abrahimyaseen2811@gmail.com>
2025-02-12 14:51:18 +0200

    AddHart

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -96,7 +96,7 @@ const Footer = () => {
       {/* bottom part */}
       <div className="mt-8 w-[80%] mx-auto border-t pt-8 flex flex-col md:flex-row justify-between items-center text-gray-600 text-sm">
         <p className="text-center md:text-left">
-          Copyright &copy; 2025 WebDev. All rights By (Ibrahim Yassin).
+          Copyright &copy; 2025 WebDev. All rights By (❤️ Ibrahim Yassin ❤️).
         </p>
         <div className="flex items-center space-x-4 mt-4 md:mt-0">
           <span>Social: </span>
